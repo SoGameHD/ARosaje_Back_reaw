87a92056bf964d88ca1b7a8c503d96797e130a9c
Anymok <arthur.heude@laposte.net>
2023-02-08 11:42:49 +0100

    update db

---
+++ src/main/java/org/arosaje/repository/PlantRepository.java
@@ -0,0 +1,10 @@
+package org.arosaje.repository;
+
+import org.arosaje.entities.Plant;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlantRepository extends CrudRepository<Plant, Integer> {
+
+
+
+}
