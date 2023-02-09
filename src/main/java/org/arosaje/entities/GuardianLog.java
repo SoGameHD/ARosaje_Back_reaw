2865cefd1864b9428c2025be241f88fd3e71931b
Anymok <arthur.heude@laposte.net>
2023-02-09 09:44:36 +0100

    #5 : user & plant relation

---
+++ src/main/java/org/arosaje/entities/GuardianLog.java
@@ -0,0 +1,69 @@
+package org.arosaje.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "HISTORIQUE_GARDIENNAGE")
+public class GuardianLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "UTILISATEUR_ID")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "PLANTE_ID")
+    private Plant plant;
+    @Column(name = "DATE_DEBUT")
+    private LocalDate start_date;
+    @Column(name = "DATE_FIN")
+    private LocalDate end_date;
+
+    public GuardianLog() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(LocalDate start_date) {
+        this.start_date = start_date;
+    }
+
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(LocalDate end_date) {
+        this.end_date = end_date;
+    }
+
+    @Override
+    public String toString() {
+        return "GuardianLog{" +
+                "id=" + id +
+                ", user=" + user +
+                ", plant=" + plant +
+                ", start_date=" + start_date +
+                ", end_date=" + end_date +
+                '}';
+    }
+}
--- src/main/java/org/arosaje/repository/PlantRepository.java
+++ src/main/java/org/arosaje/repository/PlantRepository.java
@@ -2,9 +2,5 @@ package org.arosaje.repository;
 
 import org.arosaje.entities.Plant;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
-
-import java.util.List;
 
 public interface PlantRepository extends CrudRepository<Plant, Integer> {}
