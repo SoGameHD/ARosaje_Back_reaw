eb501d6adc196cd61beeb6e301977033e60e10e0
Arthur HEUDE <arthur.heude@laposte.net>
2023-03-01 12:09:56 +0100

    update PlantController

--- src/main/java/org/arosaje/controller/PlantController.java
+++ src/main/java/org/arosaje/controller/PlantController.java
@@ -1,18 +1,22 @@
 package org.arosaje.controller;
 
 import org.arosaje.entities.Plant;
+import org.arosaje.repository.PlantRepository;
 import org.arosaje.service.PlantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class PlantController {
     @Autowired
     private PlantService plantService;
+
+    @Autowired
+    private PlantRepository plantRepository;
 
     @PostMapping( "/addPlant" )
     public void addPlant(@RequestParam Integer ownerId, @RequestBody Plant plant) {
@@ -27,5 +31,10 @@ public class PlantController {
     @GetMapping( "/getPlants" )
     public List<Plant> getPlants() {
         return plantService.findAll();
+    }
+
+    @GetMapping( "/getPlant/{id}" )
+    public Optional<Plant> getPlant(@PathVariable Integer id) {
+        return plantRepository.findById(id);
     }
 }
